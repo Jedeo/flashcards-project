8a19ef57ff245baa1494d300166a226e49f9452e
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-08-24 08:24:36 -0600

    Ref: deck-test file

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,17 +1,21 @@
 const { expect } = require("chai")
-
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
 const Card = require('../src/Card')
 const Deck = require('../src/Deck')
 
 describe('Deck', () => {
+    let cards;
+    let deck;
+    beforeEach( () => {
+         cards = prototypeQuestions.map(card => new Card(card.id,card.question,card.answers,card.correctAnswer))
+         deck = new Deck(cards)
+    })
+
     it('should know how many Cards are in the Deck', () => {
-        const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-        const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-        const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-       
-        let decks = new Deck([card1, card2, card3])
+        let decks = new Deck(cards)
         let result = decks.countCards();
 
-        expect(result).to.equal(3)
+        expect(result).to.equal(30)
     });
 })
